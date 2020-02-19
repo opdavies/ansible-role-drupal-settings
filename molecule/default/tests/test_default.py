2cf2b919e20271fe4cd33c9f2661ed98453ac59b
Oliver Davies <oliver@oliverdavies.uk>
2020-02-19 00:25:31 +0000

    Copy molecule tests from next branch
    Add more tests in follow-up PRs.
    
    Fixes #6

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_drupal_site_directory_exists(host):
+    directory = host.file('/var/www/test/sites/default')
+
+    assert directory.exists
+    assert directory.is_directory
+
+def test_drupal_settings_file_exists(host):
+    file = host.file('/var/www/test/sites/default/settings.test.php')
+
+    assert file.exists
+    assert file.is_file
